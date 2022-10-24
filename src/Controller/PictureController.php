141eb349b132f1f0fbf30fbe22c80cdf9f69baca
Thomas Noel <thomas.noel612@gmail.com>
2022-10-24 16:36:47 +0200

    picture

--- src/Controller/PictureController.php
+++ src/Controller/PictureController.php
@@ -3,30 +3,37 @@
 namespace App\Controller;
 
 use App\Entity\Picture;
+use App\Repository\PictureRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class PictureController extends AbstractController
 {
-    #[Route('/picture', name: 'app_picture')]
-    public function index(): JsonResponse
+    #[Route('api/pictures/{idPicture}', name: 'picture.get', methods:['GET'])]
+    public function getPicture(int $idPicture, SerializerInterface $serializer, PictureRepository $pictureRepository, UrlGeneratorInterface $urlGenerator): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PictureController.php',
-        ]);
+        $picture = $pictureRepository->find($idPicture);
+        //$location = $picture->getPublicPath();
+        if($picture){
+           return new JsonResponse($serializer->serialize($picture, 'json', ["groups" => 'getPicture']), JsonResponse::HTTP_OK, [], true);
+        }
+        return new JsonResponse(null, JsonResponse::HTTP_NOT_FOUND);
+        
     }
 
     #[Route('api/pictures', name: 'pictures.create', methods:['POST'])]
-    public function createPicture(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    public function createPicture(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer, UrlGeneratorInterface $urlGenerator): JsonResponse
     {
+
         $picture = new Picture();
-        $picture->setPublicPath('test');
+        $picture->setPublicPath("/assets/pictures");
         $picture->setStatus('ON');
         $files = $request->files->get('file');
         $picture->setFile($files);
@@ -34,9 +41,9 @@ class PictureController extends AbstractController
         $picture->setRealName($files->getClientOriginalName());
         $entityManager->persist($picture);
         $entityManager->flush();
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PictureController.php',
-        ]);
+
+        $location = $urlGenerator->generate('picture.get', ['idPicture' => $picture->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+        $jsonPicture = $serializer->serialize($picture, "json", ['groups' => 'getPicture']);
+        return new JsonResponse($jsonPicture, JsonResponse::HTTP_CREATED, ['Location' => $location], true);
     }
 }
--- src/Entity/Picture.php
+++ src/Entity/Picture.php
@@ -6,6 +6,7 @@ use App\Repository\PictureRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\File;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: PictureRepository::class)]
 /**
@@ -16,18 +17,23 @@ class Picture
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['getPicture', 'getAllPicture'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['getPicture', 'getAllPicture'])]
     private ?string $realName = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['getPicture', 'getAllPicture'])]
     private ?string $realPath = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['getPicture', 'getAllPicture'])]
     private ?string $publicPath = null;
 
     #[ORM\Column(length: 50)]
+    #[Groups(['getPicture', 'getAllPicture'])]
     private ?string $mimeType = null;
 
     #[ORM\Column(length: 20)]
